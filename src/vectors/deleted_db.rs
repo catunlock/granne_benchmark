ca5fafce20f662ddc97bd7de8fc499c7c1019cd9
Alberto López Sánchez <alberto.lopez.s@outlook.es>
2021-12-22 20:08:06 +0100

    add filter batch method

--- src/vectors/deleted_db.rs
+++ src/vectors/deleted_db.rs
@@ -25,7 +25,7 @@ impl<'a> DeletedDBReader<'a> {
         })
     }
 
-    pub fn contains(&self, idx: usize) -> Result<bool, lmdb::Error> {
+    pub fn _contains(&self, idx: usize) -> Result<bool, lmdb::Error> {
         trace!("Check if contains: {}", idx);
         let key = bincode::serialize(&idx).unwrap();
         let env = self.db.env();
@@ -35,11 +35,25 @@ impl<'a> DeletedDBReader<'a> {
 
         match access.get::<[u8], [u8]>(&self.db, &key) {
             Ok(_) => Ok(true),
-            Err(e) => {
-                error!("Error access {:?}: {}", key, e.to_string());
-                Ok(false)
-            },
+            Err(e) => Ok(false)
         }
+    }
+
+    pub fn filter(&self, idxs: &[usize]) -> Result<Vec<usize>, lmdb::Error> {
+        trace!("Filtering indexes");
+        let env = self.db.env();
+        let txn = lmdb::ReadTransaction::new(env).unwrap();
+        let access = txn.access();
+
+        Ok(idxs.iter().filter(|idx| {
+            let key = bincode::serialize(&idx).unwrap();
+            match access.get::<[u8], [u8]>(&self.db, &key) {
+                Ok(_) => false,
+                Err(e) => true
+            }
+        })
+        .map(|idx| *idx)
+        .collect())
     }
 }
 
@@ -122,16 +136,18 @@ mod test {
         init();
         let tempdir = tempdir().unwrap();
         let path = tempdir.path().to_str().unwrap();
-        let mut writer = DeletedDBWriter::open("db2").unwrap();
-        let mut reader = DeletedDBReader::open("db2").unwrap();
+        let writer = DeletedDBWriter::open(path).unwrap();
+        let reader = DeletedDBReader::open(path).unwrap();
 
         writer.add(1).unwrap();
         writer.add(2).unwrap();
         writer.add(3).unwrap();
         writer.add(256).unwrap();
 
-        assert!(reader.contains(1).unwrap());
-        assert!(reader.contains(256).unwrap());
+        assert!(reader._contains(1).unwrap());
+        assert!(reader._contains(256).unwrap());
+
+        assert_eq!(reader.filter(&[1,2,3,4,5,6,256]).unwrap(), [4,5,6]);
     }
 
     #[test]
@@ -151,7 +167,7 @@ mod test {
 
         std::thread::spawn(move || {
             for i in 0..1_000 {
-                assert!(reader.contains(i).unwrap());
+                assert!(reader._contains(i).unwrap());
             }
         }).join().unwrap();
     }
@@ -170,7 +186,7 @@ mod test {
 
         std::thread::spawn(move || {
             for i in 0..1_000 {
-                assert!(reader.contains(i).unwrap());
+                assert!(reader._contains(i).unwrap());
             }
         }).join().unwrap();
     }
